78008386577459899d37e68c5332a448370bd286
Yasin Kaya <cihanyasinkaya@users.noreply.github.com>
2022-04-30 16:06:10 +0300

    dark mode kalktı

--- src/components/right-sidebar/index.jsx
+++ src/components/right-sidebar/index.jsx
@@ -1,21 +1,23 @@
 import SearchIcon from "../assets/logos/right-sidebar-logo"
 
 const RightSideBar = () => (
+
   <aside class="w-64" aria-label="Sidebar">
     <div class="overflow-y-auto py-4 px-3 rounded">
       <form>
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only text-gray-300">Search</label>
         <div class="relative">
           <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <SearchIcon />
           </div>
-          <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search Mockups, Logos..." required />
-          <SearchIcon />
+          <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+
         </div>
       </form>
     </div>
 
     <div>
-      <div className="block overflow-hidden w-full text-sm text-gray-900 bg-[#16181c] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500  ">
+      <div className="block overflow-hidden w-full text-sm text-gray-900 bg-[#16181c] rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500">
         <h2 className="font-bold text-xl p-5 pl-5 pr-5">İlgini çekebilecek gündemler</h2>
         <ul class="list-none">
           <li className="grid grid-rows-12 mt-3 hover:bg-[#2e3239] pt-2 pl-5 pr-5 pb-2 ">
@@ -81,4 +83,4 @@ const RightSideBar = () => (
 
 )
 
-export default RightSideBar
+export default RightSideBar 
